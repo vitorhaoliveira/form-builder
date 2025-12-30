80211dc0bd0d38b65fc4526fb9d4f5883a327f5a
vitorhaoliveira <vitorhaoliveira123@gmail.com>
2025-12-30 18:37:39 -0300

    fix: deploy vercel

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ["@form-builder/ui", "@form-builder/database", "@form-builder/email"],
+  transpilePackages: ["@form-builder/ui", "@form-builder/database", "@form-builder/email", "@form-builder/config"],
 };
 
 export default nextConfig;
